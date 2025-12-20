1aa722ff0a16dd3df38b4634f1316c80d6252e49
ayngaran04 <ayngarannn@gmail.com>
2025-12-20 12:16:05 +0530

    first commit

---
+++ src/main/java/com/ayngaran/smart_rate_limiter/SmartRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.ayngaran.smart_rate_limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRateLimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartRateLimiterApplication.class, args);
+    }
+
+}
