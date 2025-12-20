3592d86a1ce54aa7182c0a6ac4afcd480daf89a2
ayngaran04 <ayngarannn@gmail.com>
2025-12-21 01:18:59 +0530

    minor change in the interface

--- src/main/java/com/ayngaran/smart_rate_limiter/Service/Algorithms/RateLimiter.java
+++ src/main/java/com/ayngaran/smart_rate_limiter/Service/Algorithms/RateLimiter.java
@@ -1,4 +1,4 @@
-package com.ayngaran.smart_rate_limiter.Middleware;
+package com.ayngaran.smart_rate_limiter.Service.Algorithms;
 
 public interface RateLimiter {
     boolean allowRequest(String id);
