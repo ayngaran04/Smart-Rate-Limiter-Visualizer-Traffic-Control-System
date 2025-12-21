69080adb2ed716a62d4e35bec25536a2a4a98d15
ayngaran04 <ayngarannn@gmail.com>
2025-12-21 18:19:25 +0530

    added leaky bucket algorithm

---
+++ src/main/java/com/ayngaran/smart_rate_limiter/Service/Algorithms/LeakyBucketAlgorithm.java
@@ -0,0 +1,50 @@
+package com.ayngaran.smart_rate_limiter.Service.Algorithms;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class LeakyBucketAlgorithm implements RateLimiter {
+
+    public static class Bucket {
+        Queue<Long> queue = new ConcurrentLinkedQueue<>();
+        long lastLeakTime = System.currentTimeMillis();
+    }
+
+    private final int capacity;
+    private final int reqPerSecond;
+    private final ConcurrentHashMap<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    public LeakyBucketAlgorithm(int capacity, int reqPerSecond) {
+        this.capacity = capacity;
+        this.reqPerSecond = reqPerSecond;
+    }
+
+    private void process(String id) {
+        Bucket bucket = buckets.get(id);
+        long now = System.currentTimeMillis();
+
+        if (now - bucket.lastLeakTime >= 1000) {
+            int counter = 0;
+            while (!bucket.queue.isEmpty() && counter < reqPerSecond) {
+                bucket.queue.poll();
+                counter++;
+            }
+            bucket.lastLeakTime = now;
+        }
+    }
+
+    @Override
+    public boolean allowRequest(String id) {
+        buckets.putIfAbsent(id, new Bucket());
+        Bucket bucket = buckets.get(id);
+
+        if (bucket.queue.size() >= capacity) {
+            return false;
+        }
+
+        bucket.queue.add(System.currentTimeMillis());
+        process(id);
+        return true;
+    }
+}
